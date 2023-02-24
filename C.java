7e6628b44d60acb835352c8dd76a5102f1ca3e70
LAPTOP-VVN5UJMH\kamle <kamleshkumbhare263@gmail.com>
2023-02-24 16:14:21 +0530

    c file changes1

--- C.java
+++ C.java
@@ -6,10 +6,6 @@ class C {
 		method6() {
 			System.out.println("in method6 class C");
 		}
-
-		method12() {
-			System.out.println("in method12 class C");
-		}
 	
 		method15() {
 			System.out.println("in method15 class C");
