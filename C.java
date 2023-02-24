3a41c863366db5b6b3b608d704ef577fc1a620a0
LAPTOP-VVN5UJMH\kamle <kamleshkumbhare263@gmail.com>
2023-02-24 16:41:16 +0530

    this is changes in experiment

--- C.java
+++ C.java
@@ -7,8 +7,12 @@ class C {
 			System.out.println("in method6 class C");
 		}
 	
-		method15() {
-			System.out.println("in method15 class C");
+		method12() {
+			System.out.println("in method12 class C");
+		}
+
+		method10() {
+			System.out.println("in method10 class C");
 		}
 
 		method11() {
