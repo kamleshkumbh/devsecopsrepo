1802913ecd6cd123222ab408a0f0d1e094fe54c6
LAPTOP-VVN5UJMH\kamle <kamleshkumbhare263@gmail.com>
2023-02-24 15:05:56 +0530

    new experiments

--- C.java
+++ C.java
@@ -3,4 +3,8 @@ class C {
 			System.out.println("in method5 class C");
 		}
 
+		method6() {
+			System.out.println("in method6 class C");
+		}
+
 }
---
+++ D.java
@@ -0,0 +1,7 @@
+class D {
+		method7() {
+			System.out.println("in method7 class D");
+		}
+
+
+}
